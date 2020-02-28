91e79f7bd7cedba07897bb89c4fe18f4feed2362
MathiasSamaniego <61044688+MathiasSamaniego@users.noreply.github.com>
2020-02-28 15:02:01 +0100

    Test push

---
+++ server/connectionPool/JDBCConnectionPool.java
@@ -0,0 +1,36 @@
+package connectionPool;
+
+import java.util.ArrayList;
+import java.sql.*;
+
+public class JDBCConnectionPool {
+
+	
+
+	private ArrayList<Connection> a;
+
+	public void fill() throws SQLException { 		// remplir l'attribut
+		//try {Class.forName("url");} // driver chargé 
+		for(int i = 1 ; i <= 10 ; i++) {
+			a.add(DriverManager.getConnection("url", "user" + i, "password" + i)); // connexion
+			Statement s = a.get(i).createStatement (); // statement créé
+		} 
+	}
+	
+	public Connection take(int i) { 		// prend un objet de l'attribut
+	
+		Connection cp = a.get(i);
+		a.remove(i);
+		return cp;
+	} 
+
+	void restore(Connection cp) {
+		a.add(cp);
+	} // rendre la connexion
+
+	void closeConnection() throws SQLException {
+		for(Connection y : a) {
+			y.close();
+		}
+	} // ferme les connexion de l'attribut
+}
