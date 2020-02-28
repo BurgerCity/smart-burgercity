8e15d9ff488030a932bb8f03ef8c7a599fba03ab
MathiasSamaniego <61044688+MathiasSamaniego@users.noreply.github.com>
2020-02-28 16:18:13 +0100

    Creation data source

---
+++ server/connectionPool/DataSource.java
@@ -0,0 +1,18 @@
+package connectionPool;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSource {
+	private static JDBCConnectionPool connection;
+	
+	public static Connection returnConnection() {
+		return connection.take();
+	}
+	 public static void addConnection(Connection c) {
+		 connection.restore(c);
+	 }
+	 public static void closeC() throws SQLException{
+		 connection.closeConnection();
+	 }
+}
