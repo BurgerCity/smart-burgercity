352d130da1201b4981fd0c1f03f82631de3a7ed8
MathiasSamaniego <61044688+MathiasSamaniego@users.noreply.github.com>
2020-02-28 18:00:43 +0100

    Creation property

--- server/connectionPool/PropertyLoader.java
+++ server/connectionPool/PropertyLoader.java
@@ -5,7 +5,16 @@ import java.io.IOException;
 import java.io.*;
 
 public class PropertyLoader {
+	Properties prop = new Properties();
+	InputStream input = null;
 	public void load(InputStream inStream) throws IOException 	{
+		try {
+			input = new FileInputStream("/smart-burgercity/server/connectionPool/ressources.properties");
+			prop.load(input);
+			
+		} catch (Exception e) {}
+	}
+	public static String getProperty( String propertyName ) {
 		
 	}
 }
