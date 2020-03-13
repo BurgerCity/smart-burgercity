1840d159291593e795fb3097a9d87608fdecaede
MathiasSamaniego <61044688+MathiasSamaniego@users.noreply.github.com>
2020-03-13 16:51:36 +0100

    DERNIERE VERSION AVANT DEMO

--- src/connectionPool/JDBCConnectionPool.java
+++ src/connectionPool/JDBCConnectionPool.java
@@ -15,12 +15,10 @@ public class JDBCConnectionPool {
 		try {
 			prop.loaded();
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 			System.out.println("error properties");
 		}
 		try {
-
 			for(int i = 1 ; i <= 10 ; i++) {
 				Class.forName(prop.getProperty("driver")); // loaded the driver (use properties)
 				cn=DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"), prop.getProperty("password"));
@@ -28,28 +26,17 @@ public class JDBCConnectionPool {
 			}
 			prop.close();
 		} catch (Exception e){
-			System.out.println("erreur for");
+			System.out.println("error connection");
 		}
 	}
 	
-	public ArrayList<Connection> getA() {
-		return a;
-	}
-
-	public void setA(ArrayList<Connection> a) {
-		this.a = a;
-	}
-
-	public Connection take()  { // To take a object of the attribute
+	public Connection take()  { // To take an object of the attribute
 		Connection cp=null;
 		if(!a.isEmpty()) {
 			cp= a.get(0);
 			a.remove(0);
-			return cp;
-			}
-		else {
-			return cp;
-		} 
+		}
+		return cp;
 	}
 
 	void restore(Connection cp) {		// return the connection
