ed1d557e2c59f6970bef2c38a74fc5971529c60d
MathiasSamaniego <61044688+MathiasSamaniego@users.noreply.github.com>
2020-03-27 15:13:32 +0100

    Select Update

---
+++ src/client/Select.java
@@ -0,0 +1,13 @@
+package client;
+
+public class Select {
+	private String table;
+
+	public String getTable() {
+		return table;
+	}
+
+	public void setTable(String table) {
+		this.table = table;
+	}
+}
