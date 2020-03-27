7b116a9c7d7ca2f6859db4e1cab934498e849255
idrisszerai <idriss.zeai@gmail.com>
2020-03-27 15:16:28 +0100

    15:16

---
+++ src/client/Insert.java
@@ -0,0 +1,27 @@
+package client;
+
+public class Insert {
+	private String table;
+	private String firstname;
+	private String lastname;
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getTable() {
+		return table;
+	}
+
+	public void setTable(String table) {
+		this.table = table;
+	}
+	
+}
