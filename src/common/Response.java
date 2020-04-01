4d7e9fea965944640c74f764946a9e10b9913f4b
idrisszerai <idriss.zeai@gmail.com>
2020-04-01 20:03:54 +0200

    20 03

--- src/common/Response.java
+++ src/common/Response.java
@@ -4,10 +4,17 @@ public class Response {
 	Boolean successfulOperation = true;
 	String typeOperation;
 	String select;
+	String Connection_Status;
 	
 	public Response() {
 	}
-	
+	public String getConnection_Status() {
+		return Connection_Status;
+	}
+
+	public void setConnection_Status(String connection_Status) {
+		Connection_Status = connection_Status;
+	}
 	public Boolean getSuccessfulOperation() {
 		return successfulOperation;
 	}
