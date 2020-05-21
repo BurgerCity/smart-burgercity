da64d1e35fdcbe368366cd7a2e71ff5aa732e5ed
MarineBucaille <61044715+MarineBucaille@users.noreply.github.com>
2020-05-22 00:42:48 +0200

    Add files via upload

--- src/client/Tram.java
+++ src/client/Tram.java
@@ -4,12 +4,17 @@ package client;
  */
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
 
 
 public class Tram extends JPanel {
-	  public static void main(String[] args) {
+	  public static void main(String[] args) throws JsonMappingException, JsonProcessingException, IOException {
 	  JFrame frame = new JFrame();
 	    frame.setTitle("Your tram network");
 	    frame.setSize(1600, 800);
@@ -21,12 +26,14 @@ public class Tram extends JPanel {
 	    });
 	    Container contentPane = frame.getContentPane();
 	    contentPane.add(new wind());
+	   // Station s = new Station();
+	  //  System.out.println(s.height);
 	    
 	    frame.setVisible(true);
 
 	    
 	 }
 
-
+ 
 	}
 
