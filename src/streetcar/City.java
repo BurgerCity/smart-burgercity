599e9ad43f64866f5e63eb565e30a34a659d56db
MarineBucaille <61044715+MarineBucaille@users.noreply.github.com>
2020-05-22 17:08:56 +0200

    Add files via upload

---
+++ src/streetcar/City.java
@@ -0,0 +1,22 @@
+package streetcar;
+// this is a test // Change datas here
+import javax.swing.JOptionPane;
+
+class City {
+	 // String a_dialogue = JOptionPane.showInputDialog("Hauteur de la ville");
+
+	  private int a = 5;  /*width*/
+	  private int b = 10;   /*Height*/
+
+	  private int budget =200("Budget de la ville"));  /*city's budget in €*/
+	  private int station_price =10;  /*price to build a station*/
+	  private double surface = (Math.PI*a*b);  /*surface of the city*/
+	  public int getA(){return a;}                                  /**/
+	  public int getB(){return b;}                                  /**/
+	  public int getBudget(){return budget;}                        /*definition of get methods*/
+	  public int getStation_price(){return station_price;}          /**/
+	  public double getSurface(){return surface;}                     /**/
+
+
+
+}
